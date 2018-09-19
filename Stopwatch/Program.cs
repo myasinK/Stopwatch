ce0628b05f4cb8ec5dae26fc6a30e65ddbb81511
myasinK <yasinul@gmail.com>
2018-09-19 00:49:09 -0700

    Build stopwatch skeleton

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mosh
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            System.Threading.Thread.Sleep(5000);
+            stopwatch.Stop();
+            Console.WriteLine("Time: {0}seconds", stopwatch.Duration);
+            stopwatch.Start();
+            stopwatch.Reset();
+            stopwatch.Start();
+            System.Threading.Thread.Sleep(2000);
+            stopwatch.Stop();
+            Console.WriteLine("Time: {0}seconds", stopwatch.Duration);
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Mosh
+{
+    public class Stopwatch
+    {
+        private bool _isStarted;
+        private DateTime _startTime;
+        private DateTime _stopTime;
+
+        private TimeSpan TimeSpan
+        {
+            get => _stopTime - _startTime;
+            set => TimeSpan = _stopTime - _startTime;
+        }
+
+        public int Duration
+        {
+            get
+            {
+                var duration = (_stopTime - _startTime).Seconds;
+                return duration;
+
+            }
+        }
+
+        public Stopwatch()
+        {
+            _isStarted = false;
+        }
+
+
+        public void Start()
+        {
+            if (!_isStarted)
+            {
+                _isStarted = true;
+                _startTime = (Duration > 0) ? _startTime : DateTime.Now;
+                Console.WriteLine("Stopwatch started");
+
+            }
+            else
+            {
+                throw new InvalidOperationException("STOPWATCH ALREADY STARTED");
+            }
+        }
+
+        public void Stop()
+        {
+            if (_isStarted)
+            {
+                _isStarted = false;
+                _stopTime = DateTime.Now;
+                Console.WriteLine("Stopwatch stopped");
+            }
+            else
+            {
+                throw new InvalidOperationException("STOPWATCH ALREADY STOPPED");
+            }
+        }
+
+        public void Reset()
+        {
+            _isStarted = false;
+            _startTime = DateTime.Now;
+            _stopTime = _startTime;
+            Console.WriteLine("Stopwatch reset");
+        }
+    }
+}
